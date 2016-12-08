ee9de92fa8255f6cf3236318770e18dbc83bf73b
Kreativadelar <patrik.bichis@kreativadelar.se>
2016-12-08 13:19:57 +0100

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Empty JS for your own code to be here
+$(function(){
+    $('#control').click(function(){
+        window.location=('control.html');
+    });
+});
+
+$(function(){
+    $('#menu').click(function(){
+        window.location=('menu.html');
+    });
+});
+
+$(function(){
+    $('#dashboard').click(function(){
+        window.location=('index.html?load=viewer.json');
+    });
+});
